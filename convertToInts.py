7efe7a7407dd541f9927b544873ed630b122688b
grebnesorbocaj <bocajgrebnesor@gmail.com>
2022-09-14 19:57:23 -0600

    dev: remove shadow vars

--- convertToInts.py
+++ convertToInts.py
@@ -5,26 +5,26 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def linkedToList(l1: Optional[ListNode]) -> Optional[int]:
+        def linkedToList(tmp_lst: Optional[ListNode]) -> Optional[int]:
             res = []
-            while l1:
-                res.append(l1.val)
-                l1 = l1.next
+            while tmp_lst:
+                res.append(tmp_lst.val)
+                tmp_lst = tmp_lst.next
             return res
         
-        l1 = linkedToList(l1)
-        l2 = linkedToList(l2)
+        l1_list = linkedToList(l1)
+        l2_list = linkedToList(l2)
         
         
-        def listToInt(l1: Optional[int]) -> int:
-            for i in range(len(l1)):
-                l1[i] = str(l1[i])
-            intStr = "".join(l1)
+        def listToInt(tmp_lst: Optional[int]) -> int:
+            for i in range(len(tmp_lst)):
+                tmp_lst[i] = str(tmp_lst[i])
+            intStr = "".join(tmp_lst)
             return int(intStr)
         
-        l1 = listToInt(l1)
-        l2 = listToInt(l2)
-        tot = int(l1) + int(l2)
+        l1_int = listToInt(l1_list)
+        l2_int = listToInt(l2_list)
+        tot = int(l1_int) + int(l2_int)
         
         
         def intToList(val: int) -> Optional[int]:
@@ -33,14 +33,14 @@ class Solution:
                 lst[i] = int(lst[i])
             return lst
         
-        tot = intToList(tot)
+        tot_list = intToList(tot)
         
-        def listToLinked(lst: Optional[int]) -> Optional[ListNode]:
+        def listToLinked(tmp_lst: Optional[int]) -> Optional[ListNode]:
             head = curr = ListNode()
-            for n in lst:
+            for n in tmp_lst:
                 curr.next = ListNode(n)
                 curr = curr.next
             return head.next
         
-        return listToLinked(tot)
+        return listToLinked(tot_list)
         
